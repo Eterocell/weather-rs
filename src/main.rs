5bfb86d8c574de61785868835a4339536a2399f7
Eterocell <eterocell@outlook.com>
2021-03-13 15:42:11 +0800

    Add some color to temperature display

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use weather_rs::{print_response, Input};
 
 #[tokio::main]
 async fn main() -> Result<(), ExitFailure> {
+    #[allow(unused)]
+    let enabled = ansi_term::enable_ansi_support();
     let input = Input::from_args();
     let response = Response::get(&input.city).await?;
     print_response(response);
